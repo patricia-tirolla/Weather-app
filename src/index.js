ffea55874e452f90d9566591c4b9732cb009d338
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-11-08 13:11:56 +0100

    Refactor functions and objects

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 
 
-function fetchAndDisplayData() {
+function createLocationFetcher(callback) {
 
     return {
-        tempToday: "",
-        tempMin: "",
-        tempMax: "",
-        temp15Days: "",
-        date: "",
 
         locationInput: "",
 
@@ -21,64 +16,44 @@ function fetchAndDisplayData() {
                 .then(function (response) {
                     return response.json();
                 })
-                // the arrow function allows to use the parent scope
-                .then((json) => {
-                    // test log (delete)
-                    console.log(json);
 
-                    this.tempToday = (json.days[1].temp);
-                    this.tempMin = (json.days[1].tempmin);
-                    this.tempMax = (json.days[1].tempmax);
-
-                    const length = (json.days.length)
-                    for (let i = 0; i < length; i++) {
-                        this.temp15Days = (json.days[i].temp)
-                        this.date = (json.days[i].datetime);
-
-                        console.log(this.date + ": " + this.temp15Days)
-                    }
-                    this.displayFetchData(json.days);
-
-                    // test log (delete)
-                    console.log("The temperature today in " + this.locationInput + " is " + this.tempToday)
-                    console.log("The temperature today in " + this.locationInput + " is " + this.tempMin)
-                    console.log("The temperature today in " + this.locationInput + " is " + this.tempMax)
+                .then((json) => { 
+                    callback(this.locationInput, json.days)
                 })
         },
-
-    displayFetchData(list) {
-            const template = document.getElementById("template");
-            const displayContainer = document.getElementById("display");
-            const clone = template.content.cloneNode(true);
-
-            clone.getElementById("city-name").textContent = this.locationInput;
-            clone.getElementById("temperature").textContent = "The temperature today in " + this.locationInput + " is " + this.tempToday + "°F";
-            clone.getElementById("min-temp").textContent = "The min temperature today in " + this.locationInput + " is " + this.tempMin + "°F";
-            clone.getElementById("max-temp").textContent = "The max temperature today in " + this.locationInput + " is " + this.tempMax + "°F";
-
-            let i = 0;
-            for (let day of list) {
-                let liCreation = document.createElement("li");
-                liCreation.textContent = this.date + " : " + this.temp15Days;
-                i++;
-                clone.getElementById("forecast-list").appendChild(liCreation);
-            }
-
-            displayContainer.appendChild(clone);
-        }
     }
 }
 
-const fetchAndDisplay = fetchAndDisplayData();
+function display(location, days){
 
+    const template = document.getElementById("template");
+        const displayContainer = document.getElementById("display");
+        const clone = template.content.cloneNode(true);
+
+        clone.getElementById("city-name").textContent = location;
+        clone.getElementById("temperature").textContent = "The temperature today in " + location + " is " + days[0].temp + "°F";
+        clone.getElementById("min-temp").textContent = "The min temperature today in " + location + " is " + days[0].tempmin + "°F";
+        clone.getElementById("max-temp").textContent = "The max temperature today in " + location + " is " + days[0].tempmax + "°F";
+
+        for (let day of days) {
+            let liCreation = document.createElement("li");
+            liCreation.textContent = day.datetime + " : " + day.temp + "°F";
+            clone.getElementById("forecast-list").appendChild(liCreation);
+        }
+
+        displayContainer.appendChild(clone);
+}
 
 // Submit form
-function searchSubmit() {
+(function() {
+
+    const fetchAndDisplayObject = createLocationFetcher(display);
+
     const searchButton = document.getElementById("location-form");
     searchButton.addEventListener("submit", (e) => {
         e.preventDefault();
-        fetchAndDisplay.locationFetch();
+        fetchAndDisplayObject.locationFetch();
     })
-}
-searchSubmit();
+})()
+
 
